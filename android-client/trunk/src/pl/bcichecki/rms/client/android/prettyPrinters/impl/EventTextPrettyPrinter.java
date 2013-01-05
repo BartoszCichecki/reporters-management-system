e598647f1d08f79615b1d49b78e79a24e52748de
cichy202@gmail.com <cichy202@gmail.com@a1e89e27-1ae0-082d-aede-8017c347ddc4>
2013-01-05 00:47:31 +0000

    [and] Implemented EventTextPrettyPrinter

--- android-client/trunk/src/pl/bcichecki/rms/client/android/prettyPrinters/impl/EventTextPrettyPrinter.java
+++ android-client/trunk/src/pl/bcichecki/rms/client/android/prettyPrinters/impl/EventTextPrettyPrinter.java
@@ -11,13 +11,17 @@
 
 package pl.bcichecki.rms.client.android.prettyPrinters.impl;
 
-import java.util.List;
+import java.util.Locale;
 
 import android.content.Context;
 
+import pl.bcichecki.rms.client.android.R;
+import pl.bcichecki.rms.client.android.model.impl.Device;
 import pl.bcichecki.rms.client.android.model.impl.Event;
-import pl.bcichecki.rms.client.android.model.impl.Message;
+import pl.bcichecki.rms.client.android.model.impl.EventType;
+import pl.bcichecki.rms.client.android.model.impl.User;
 import pl.bcichecki.rms.client.android.prettyPrinters.AbstractPrettyPrinter;
+import pl.bcichecki.rms.client.android.utils.AppUtils;
 
 /**
  * @author Bartosz Cichecki
@@ -25,22 +29,53 @@ import pl.bcichecki.rms.client.android.prettyPrinters.AbstractPrettyPrinter;
  */
 public class EventTextPrettyPrinter extends AbstractPrettyPrinter<Event> {
 
-	@SuppressWarnings("unused")
-	private List<Message> messages;
-
 	public EventTextPrettyPrinter(Context context) {
 		super(context);
-	}
-
-	public EventTextPrettyPrinter addAditionalData(List<Message> messages) {
-		this.messages = messages;
-		return this;
 	}
 
 	@Override
 	public String print(Event event) {
 		StringBuilder sb = new StringBuilder();
-		// TODO print
+		sb.append(getContext().getString(R.string.strings_pretty_printer_event_teaser));
+		sb.append(getContext().getString(R.string.strings_pretty_printer_event_title));
+		sb.append(event.getTitle());
+		sb.append("\n");
+		sb.append(getContext().getString(R.string.strings_pretty_printer_event_type));
+		if (event.getType().equals(EventType.INTERVIEW)) {
+			sb.append(getContext().getString(R.string.strings_pretty_printer_event_type_interview));
+		}
+		if (event.getType().equals(EventType.MEETING)) {
+			sb.append(getContext().getString(R.string.strings_pretty_printer_event_type_meeting));
+		}
+		sb.append("\n");
+		sb.append(getContext().getString(R.string.strings_pretty_printer_event_starts_at));
+		sb.append(AppUtils.getFormattedDateAsString(event.getStartDate(), Locale.getDefault()));
+		sb.append(getContext().getString(R.string.strings_pretty_printer_event_ends_at));
+		sb.append(AppUtils.getFormattedDateAsString(event.getEndDate(), Locale.getDefault()));
+		sb.append("\n");
+		sb.append(getContext().getString(R.string.strings_pretty_printer_event_description));
+		sb.append(event.getDescription());
+		sb.append("\n");
+		if (event.getParticipants() != null && event.getParticipants().size() > 0) {
+			sb.append(getContext().getString(R.string.strings_pretty_printer_event_participants));
+			for (User participant : event.getParticipants()) {
+				sb.append(" - ");
+				if (participant.getAddress() != null && participant.getAddress().getFirstName() != null
+				        && participant.getAddress().getLastName() != null) {
+					sb.append(participant.getAddress().getFirstName()).append(" ").append(participant.getAddress().getLastName());
+				} else {
+					sb.append(participant.getUsername());
+				}
+				sb.append("\n");
+			}
+		}
+		if (event.getDevices() != null && event.getDevices().size() > 0) {
+			sb.append(getContext().getString(R.string.strings_pretty_printer_event_devices));
+			for (Device device : event.getDevices()) {
+				sb.append(" - ").append(device.getName()).append("\n");
+			}
+		}
+		sb.append(getContext().getString(R.string.strings_pretty_printer_event_footer));
 		return sb.toString();
 	}
 
